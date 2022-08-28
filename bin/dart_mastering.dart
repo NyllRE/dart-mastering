bd7686f30370c06696ca3f8964bb71d87ce2b2ec
NyllRE <nyll.redeast@gmail.com>
2022-08-28 23:10:37 +0300

    first commit

---
+++ bin/dart_mastering.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mastering/dart_mastering.dart' as dart_mastering;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_mastering.calculate()}!');
+}
---
+++ test/dart_mastering_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_mastering/dart_mastering.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
