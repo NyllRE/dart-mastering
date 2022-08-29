c4554719832f5847c9ef1ad3f720ac1b1ec158b8
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-08-29 23:26:39 +0300

    Finished Conditions

--- lib/conditions.dart
+++ lib/conditions.dart
@@ -1,6 +1,25 @@
 void conditions() {
   final myInt = 5;
+
+  //=== IF STATEMENTS ===//
   if (myInt == 5) {
     print("it's $myInt!");
+  } else if (myInt == 25) {
+    print("wait, how is it $myInt??");
+  } else {
+    print("welp, I don't have a clue how it's $myInt now");
+  }
+
+  //=== SWITCH STATEMENTS ===//
+  //= checks only for Equality =//
+  switch (myInt) {
+    case 5:
+      print("it's $myInt!");
+      break;
+    case 25:
+      print("how does this keep happening? who turned it into $myInt");
+      break;
+    default:
+      print("the code is cursed");
   }
 }
