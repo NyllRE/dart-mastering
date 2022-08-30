d6592ae3b438bc055e7bd5614f7827a8b4dde6ea
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-08-30 15:53:08 +0300

    Creating Custom Types

--- lib/returningFunctions.dart
+++ lib/returningFunctions.dart
@@ -1,8 +1,14 @@
-int Function(int) twice(int Function(int) f) {
-  return (int x) {
-    return f(f(x));
-  };
+//=== DECLARING A TYPE ===//
+typedef intTransformer = int Function(int);
+
+//=== NESTED FUNCTION ===//
+//= this function takes a function as args, and returns a function =//
+intTransformer twice(intTransformer f) {
+  //= this takes x as arg, runs the function given twice with the arg and returns it =//
+  return (int x) => f(f(x));
+
 }
+
 
 void returningFunctions() {
   final twicePlusFive = twice((int x) => x + 5);
