5ab6b92cb2787012da47c36a88e9f83ea2c613d9
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-08-30 16:59:50 +0300

    Extra Looping Methods

--- lib/loops.dart
+++ lib/loops.dart
@@ -1,7 +1,23 @@
 void loops() {
+  //==<< FOR LOOPS >>==//
   for (int i = 0; i < 10; i++) {
     print(i);
   }
+
+  //=> you can loop a list in a better way:
+  List<int> myList = [1, 2, 3, 4, 5];
+
+  for (final myNum in myList) {
+    print(myNum);
+  }
+
+  //=> or
+  myList.forEach((myNum) => print(myNum));
+
+  //=> dart made the code above easier py passing the function
+  myList.forEach(print);
+
+  //==<< WHILE LOOPS >>==//
 
   bool myBool = false;
   while (myBool) {
