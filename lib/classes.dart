3533e92deaef0a5608dc1e158b0c0a9c4140f23e
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-08-31 20:50:27 +0300

    Classes - Private variables & methods

--- lib/classes.dart
+++ lib/classes.dart
@@ -1,25 +1,31 @@
+import 'dart:web_gl';
+
 void classes() {
   User myuser = const User("deez", 45);
   myuser.hasLongName();
 
-  //=>> you can call static methods directly from class
+  //=>> you can call static methods directly from class <<=//
   User.longNameThreshold; //=> 10
 }
 
 class User {
   final String name;
-  final int netWorth;
+  final int _privateNetWorth;
 
   const User(
     this.name,
-    this.netWorth,
+    this._privateNetWorth,
   );
 
   bool hasLongName() {
     return name.length >= 10;
   }
 
-  //=<< static values >>=//
+  bool _privateFunction() {
+    return true;
+  }
+
+  //==<< STATIC VALUES >>==//
   //=>> can be called from outside directly <<=//
   static const longNameThreshold = 10;
 }
