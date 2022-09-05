655c81bc7536c93416bf10e35e995f80823b72b1
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-05 18:23:57 +0300

    File Parting

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,7 +1,11 @@
+part of './parts.dart'; //=> a code part of the parts.dart file to extend the extensions to there
+
 void extensions() {
   print("double ".duplicate);
 }
 
+//==<< EXTENSIONS >>==//
+//=>> you can easily add any method you like to any datatype or method by extending it
 extension StringDuplication on String {
   String get duplicate => this + this;
 }
---
+++ lib/parts.dart
@@ -0,0 +1,5 @@
+part './extensions.dart'; //=>> this can join code files to let them interact as if they were a single file "different from `import`"
+
+void parts() {
+  print("duplicate".duplicate);
+}
